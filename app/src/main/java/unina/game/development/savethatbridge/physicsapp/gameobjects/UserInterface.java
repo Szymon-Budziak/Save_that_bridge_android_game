ab2a914959e87cb869049101037d337ca79c4bab
Szymon-Budziak <szymonbudziak@gmail.com>
2023-02-07 14:38:21 +0100

    User Interface

---
+++ app/src/main/java/unina/game/development/savethatbridge/physicsapp/gameobjects/UserInterface.java
@@ -0,0 +1,59 @@
+package unina.game.development.savethatbridge.physicsapp.gameobjects;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+
+import com.google.fpl.liquidfun.BodyDef;
+import com.google.fpl.liquidfun.BodyType;
+
+import unina.game.development.savethatbridge.physicsapp.general.GameWorld;
+
+public class UserInterface extends GameObject {
+    private static int instances = 0;
+
+    private final Canvas canvas;
+    private final Paint paint;
+    private static int level = 0;
+
+    public UserInterface(GameWorld gw) {
+        super(gw);
+
+        instances++;
+
+        this.canvas = new Canvas(gw.buffer);
+        this.paint = new Paint();
+
+        // a body definition: position and type
+        BodyDef bodyDef = new BodyDef();
+        bodyDef.setType(BodyType.staticBody);
+
+        // a body
+        this.body = gw.world.createBody(bodyDef);
+        this.body.setSleepingAllowed(false);
+        this.name = "UI" + instances;
+        this.body.setUserData(this);
+
+        int color = Color.argb(255, 0, 0, 0);
+        this.paint.setColor(color);
+        this.paint.setStyle(Paint.Style.FILL_AND_STROKE);
+        this.paint.setTextSize(Math.abs(this.gw.screenSize.getyMax() / 30 - this.gw.screenSize.getyMax() / 20));
+        this.paint.setTextAlign(Paint.Align.CENTER);
+
+        // clean up native objects
+        bodyDef.delete();
+    }
+
+    public static void setLevel(int lvl) {
+        level = lvl;
+    }
+
+    @Override
+    public void draw(Bitmap buf, float x, float y, float angle) {
+        this.canvas.save();
+        this.canvas.rotate((float) Math.toDegrees(angle), x, y);
+        this.canvas.drawText("Level : " + level, this.gw.screenSize.getxMax() / 8, this.gw.screenSize.getyMax() / 30, this.paint);
+        this.canvas.restore();
+    }
+}
